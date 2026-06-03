cb81151fd05ae4cb2da96ae3fb2475705027fff0
Gabngs <gabrielsulca159@gmail.com>
2026-06-03 14:12:15 -0500

    bump version to 1.0.2

--- version.py
+++ version.py
@@ -2,7 +2,7 @@
 Información de versión para Excel Analyzer
 """
 
-__version__ = "1.0.0"
+__version__ = "1.0.2"
 __app_name__ = "Excel Analyzer"
 __author__ = "Gabngs - NeoRyu Services"
 __description__ = "Analizador y comparador de archivos Excel"
